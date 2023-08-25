c57bb8bd99fff8ce6b9895a25aed25f1b5463a31
daksh-xyz <dakshuklal@gmail.com>
2023-08-25 10:05:00 +0530

    25Aug23

--- j.c
+++ j.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
+struct node{
+    int data;
+    struct node *p;
+}*head = NULL;
 void main() {
-    int arr[] = {2, 7, 11, 15, 4, 5};
-    int target = 9;
-    int size = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < size; i++) {
-        for (int j = i + 1; j < size; j++) {
-            if (arr[i] + arr[j] == target) {
-                printf("Indices with sum %d: %d and %d\n", target, i, j);
-            }
-        }
-    }
-    printf("No indices found with sum %d.\n", target);
+    struct node *temp;
+    temp = struct
 }
